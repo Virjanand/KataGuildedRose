8b02efc0950bac30578d5fa5063c72286ac154e5
Virjanand <virjanand@virjanand.com>
2019-05-08 05:51:02 +0200

    Added test for sell date passed for regular item

--- src/test/java/com/gildedrose/RegularItemTest.java
+++ src/test/java/com/gildedrose/RegularItemTest.java
@@ -18,4 +18,15 @@ public class RegularItemTest {
         assertEquals(0, regularItem.quality);
         assertEquals(0, regularItem.sellIn);
     }
+
+    @Test
+    public void quality2SellBy0_After1Day_Quality0SellByMinus1() {
+        Item regularItem = new Item("Regular", 0, 1);
+        GildedRose inn = new GildedRose(new Item[]{regularItem});
+
+        inn.updateQuality();
+
+        assertEquals(0, regularItem.quality);
+        assertEquals(-1, regularItem.sellIn);
+    }
 }
