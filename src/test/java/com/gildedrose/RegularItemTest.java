cc74d1f6820ca1ad82b9ef88b35aadc8ae8d8206
Virjanand <virjanand@virjanand.com>
2019-05-08 06:10:02 +0200

    Refactor tests to improve given when then structure and createItem method does only 1 thing

--- src/test/java/com/gildedrose/RegularItemTest.java
+++ src/test/java/com/gildedrose/RegularItemTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,9 +9,20 @@ import static org.junit.Assert.*;
 
 public class RegularItemTest {
 
+    private Item[] items;
+    private GildedRose inn;
+
+    @Before
+    public void setUp() {
+        items = new Item[1];
+        inn = new GildedRose(items);
+    }
+
     @Test
     public void afterOneDay_qualityAndSellByMinus1() {
         Item regularItem = createItem(1, 1);
+
+        inn.updateQuality();
 
         assertEquals(0, regularItem.quality);
         assertEquals(0, regularItem.sellIn);
@@ -20,6 +32,8 @@ public class RegularItemTest {
     public void sellDatePassed_QualityMinus2SellByMinus1() {
         Item regularItem = createItem(0, 1);
 
+        inn.updateQuality();
+
         assertEquals(0, regularItem.quality);
         assertEquals(-1, regularItem.sellIn);
     }
@@ -28,14 +42,15 @@ public class RegularItemTest {
     public void qualityNeverNegative() {
         Item regularItem = createItem(1, 0);
 
+        inn.updateQuality();
+
         assertEquals(0, regularItem.quality);
         assertEquals(0, regularItem.sellIn);
     }
 
     private Item createItem(int sellBy, int quality) {
         Item regularItem = new Item("Regular", sellBy, quality);
-        GildedRose inn = new GildedRose(new Item[]{regularItem});
-        inn.updateQuality();
+        items[0] = regularItem;
         return regularItem;
     }
 }
