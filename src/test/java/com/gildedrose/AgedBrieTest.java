7849974d4daaeb062d0ed46347e78ae1a005c93a
Virjanand <virjanand@virjanand.com>
2019-05-08 06:17:44 +0200

    Create test for increase quality Aged Brie

---
+++ src/test/java/com/gildedrose/AgedBrieTest.java
@@ -0,0 +1,34 @@
+package com.gildedrose;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AgedBrieTest {
+
+    private Item[] items;
+    private GildedRose inn;
+
+    @Before
+    public void setUp() {
+        items = new Item[1];
+        inn = new GildedRose(items);
+    }
+
+    @Test
+    public void afterOneDay_qualityIncreasesAndSellByMinus1() {
+        Item regularItem = createItem(1, 1);
+
+        inn.updateQuality();
+
+        assertEquals(2, regularItem.quality);
+        assertEquals(0, regularItem.sellIn);
+    }
+
+    private Item createItem(int sellBy, int quality) {
+        Item regularItem = new Item("Aged Brie", sellBy, quality);
+        items[0] = regularItem;
+        return regularItem;
+    }
+}
